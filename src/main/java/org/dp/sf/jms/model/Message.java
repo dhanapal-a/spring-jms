fb09985d98d45da2d8f36b1ee97337b26165e310
dallimuthu01 <dallimuthu01@host.docker.internal>
2021-03-17 23:04:06 -0500

    Setting Active MQ config

---
+++ src/main/java/org/dp/sf/jms/model/Message.java
@@ -0,0 +1,19 @@
+package org.dp.sf.jms.model;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+	private UUID id;
+	private String message;
+
+}
